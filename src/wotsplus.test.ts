24eaa7fb6808e0825bde90ddda0d85fa26f98e2a
Richard T. Carback III <rick.carback@gmail.com>
2025-03-19 13:55:39 -0400

    Housekeeping and license

--- src/wotsplus.test.ts
+++ src/wotsplus.test.ts
@@ -1,3 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2024 quip.network
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
 import { describe, it, expect } from 'vitest';
 import { WOTSPlus } from './wotsplus';
 import { keccak_256 } from '@noble/hashes/sha3';
